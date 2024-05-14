a8aa26dd0bd17c7025b98d204066817d2e70747c
Christian Loef <christian@lit.one>
2024-05-14 19:04:16 +0200

    updated latest data

--- src/index.js
+++ src/index.js
@@ -5,22 +5,19 @@ import Home from './pages/home';
 import SpotList from './pages/spot-list';
 import SpotDetail from './pages/spot-detail';
 import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Home />,
   },
   {
-    path: "/spots",
+    path: '/spots',
     element: <SpotList />,
   },
   {
-    path: "/spots/detail",
+    path: '/spots/:spotId',
     element: <SpotDetail />,
   },
 ]);
@@ -29,10 +26,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
